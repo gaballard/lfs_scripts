0097bfa1d084c685dd1f8148d602e8f8748adc8d
Geoffrey Ballard <geoffreyballard@gmail.com>
2016-02-21 18:36:09 -0800

    Updated complete tool compilation script

--- x86/tools/8-tcl.sh
+++ x86/tools/8-tcl.sh
@@ -1,6 +1,185 @@
 cd $LFS/sources
+
+tar -xvf binutils-2.26.tar.bz2
+cd binutils-2.26
+mkdir -v build
+cd       build
+../configure --prefix=/tools            \
+             --with-sysroot=$LFS        \
+             --with-lib-path=/tools/lib \
+             --target=$LFS_TGT          \
+             --disable-nls              \
+             --disable-werror
+make
+case $(uname -m) in
+  x86_64) mkdir -v /tools/lib && ln -sv lib /tools/lib64 ;;
+esac
+make install
+cd ..
+rm -rf binutils-2.26
+
+tar -xvf gcc-5.3.0.tar.bz2
+cd gcc-5.3.0
+tar -xf ../mpfr-3.1.3.tar.xz
+mv -v mpfr-3.1.3 mpfr
+tar -xf ../gmp-6.1.0.tar.xz
+mv -v gmp-6.1.0 gmp
+tar -xf ../mpc-1.0.3.tar.gz
+mv -v mpc-1.0.3 mpc
+for file in \
+ $(find gcc/config -name linux64.h -o -name linux.h -o -name sysv4.h)
+do
+  cp -uv $file{,.orig}
+  sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
+      -e 's@/usr@/tools@g' $file.orig > $file
+  echo '
+#undef STANDARD_STARTFILE_PREFIX_1
+#undef STANDARD_STARTFILE_PREFIX_2
+#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
+#define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
+  touch $file.orig
+done
+mkdir -v build
+cd       build
+../configure                                       \
+    --target=$LFS_TGT                              \
+    --prefix=/tools                                \
+    --with-glibc-version=2.11                      \
+    --with-sysroot=$LFS                            \
+    --with-newlib                                  \
+    --without-headers                              \
+    --with-local-prefix=/tools                     \
+    --with-native-system-header-dir=/tools/include \
+    --disable-nls                                  \
+    --disable-shared                               \
+    --disable-multilib                             \
+    --disable-decimal-float                        \
+    --disable-threads                              \
+    --disable-libatomic                            \
+    --disable-libgomp                              \
+    --disable-libquadmath                          \
+    --disable-libssp                               \
+    --disable-libvtv                               \
+    --disable-libstdcxx                            \
+    --enable-languages=c,c++
+make
+make install
+cd ..
+rm -rf gcc-5.3.0
+
+tar -xvf linux-4.4.1.tar.xz
+cd linux-4.4.1
+make mrproper
+make INSTALL_HDR_PATH=dest headers_install
+cp -rv dest/include/* /tools/include
+cd ..
+rm -rf linux-4.4.1
+
+tar -xvf glibc-2.22.tar.xz
+cd glibc-2.22
+patch -Np1 -i ../glibc-2.22-upstream_i386_fix-1.patch
+mkdir -v build
+cd       build
+../configure                             \
+      --prefix=/tools                    \
+      --host=$LFS_TGT                    \
+      --build=$(../scripts/config.guess) \
+      --disable-profile                  \
+      --enable-kernel=2.6.32             \
+      --enable-obsolete-rpc              \
+      --with-headers=/tools/include      \
+      libc_cv_forced_unwind=yes          \
+      libc_cv_ctors_header=yes           \
+      libc_cv_c_cleanup=yes
+make
+make install
+cd ..
+rm -rf glibc-2.22
+
+tar -xvf gcc-5.3.0.tar.bz2
+cd gcc-5.3.0
+mkdir -v build
+cd       build
+../libstdc++-v3/configure           \
+    --host=$LFS_TGT                 \
+    --prefix=/tools                 \
+    --disable-multilib              \
+    --disable-nls                   \
+    --disable-libstdcxx-threads     \
+    --disable-libstdcxx-pch         \
+    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/5.3.0
+make
+make install
+cd ..
+rm -rf gcc-5.3.0
+
+tar -xvf binutils-2.26.tar.bz2
+cd binutils-2.26
+mkdir -v build
+cd       build
+CC=$LFS_TGT-gcc                \
+AR=$LFS_TGT-ar                 \
+RANLIB=$LFS_TGT-ranlib         \
+../configure                   \
+    --prefix=/tools            \
+    --disable-nls              \
+    --disable-werror           \
+    --with-lib-path=/tools/lib \
+    --with-sysroot
+make
+make install
+make -C ld clean
+make -C ld LIB_PATH=/usr/lib:/lib
+cp -v ld/ld-new /tools/bin
+cd ..
+rm -rf binutils-2.26
+
+tar -xvf gcc-5.3.0.tar.bz2
+cd gcc-5.3.0
+cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+  `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include-fixed/limits.h
+for file in \
+ $(find gcc/config -name linux64.h -o -name linux.h -o -name sysv4.h)
+do
+  cp -uv $file{,.orig}
+  sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
+      -e 's@/usr@/tools@g' $file.orig > $file
+  echo '
+#undef STANDARD_STARTFILE_PREFIX_1
+#undef STANDARD_STARTFILE_PREFIX_2
+#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
+#define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
+  touch $file.orig
+done
+tar -xf ../mpfr-3.1.3.tar.xz
+mv -v mpfr-3.1.3 mpfr
+tar -xf ../gmp-6.1.0.tar.xz
+mv -v gmp-6.1.0 gmp
+tar -xf ../mpc-1.0.3.tar.gz
+mv -v mpc-1.0.3 mpc
+mkdir -v build
+cd       build
+CC=$LFS_TGT-gcc                                    \
+CXX=$LFS_TGT-g++                                   \
+AR=$LFS_TGT-ar                                     \
+RANLIB=$LFS_TGT-ranlib                             \
+../configure                                       \
+    --prefix=/tools                                \
+    --with-local-prefix=/tools                     \
+    --with-native-system-header-dir=/tools/include \
+    --enable-languages=c,c++                       \
+    --disable-libstdcxx-pch                        \
+    --disable-multilib                             \
+    --disable-bootstrap                            \
+    --disable-libgomp
+make
+make install
+ln -sv gcc /tools/bin/cc
+cd ..
+rm -rf gcc-5.3.0
+
 tar -xvf tcl-core8.6.4-src.tar.gz
-cd tcl-core8.6.4-src
+cd tcl8.6.4
 cd unix
 ./configure --prefix=/tools
 make
@@ -9,7 +188,7 @@ chmod -v u+w /tools/lib/libtcl8.6.so
 make install-private-headers
 ln -sv tclsh8.6 /tools/bin/tclsh
 cd ..
-rm -rf tcl-core8.6.4-src
+rm -rf tcl8.6.4
 
 tar -xvf expect5.45.tar.gz
 cd expect5.45
@@ -20,7 +199,6 @@ sed 's:/usr/local/bin:/bin:' configure.orig > configure
             --with-tclinclude=/tools/include
 make
 make SCRIPTS="" install
-
 cd ..
 rm -rf expect5.45
 
